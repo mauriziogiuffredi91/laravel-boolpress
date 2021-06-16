13ece842afc8a64beeb17eee122c1a011fdc0f4d
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-06-16 20:28:31 +0200

    add: admin part

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $post = Post::all();
+        $posts = Post::all();
 
         return view('admin.posts.index', compact('posts'));
     }
